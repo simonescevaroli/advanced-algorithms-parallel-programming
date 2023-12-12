2b9e124a4081e19ced92ef883a659ace6b5dc9a5
simonescevaroli <simonescevaroli@gmail.com>
2023-12-12 14:21:40 +0100

    improved version

--- assignment2/evaluate_time.py
+++ assignment2/evaluate_time.py
@@ -3,12 +3,17 @@ import statistics
 
 # Number of time to execute the program, for more precise average time
 num_runs = 1
+if num_runs==1:
+    print(f"Computing wall time based on {num_runs} execution...")
+else:
+    print(f"Computing average wall time based on {num_runs} executions...")
 
 # Maximum number of usable processors in your computer
 max_processors = 4
+serial_time = 0
 for i in range(max_processors):
     # Command to execute the MPI program
-    mpi_command = "./scripts/launch.sh ./build/main ./data/molecules.smi output.csv " + str(i+1)
+    mpi_command = "./scripts/launch.sh ./build/main ./data/molecules_hiv.smi output.csv " + str(i+1)
     real_times = []
     
     for _ in range(num_runs):
@@ -29,6 +34,9 @@ for i in range(max_processors):
     average_real_time = statistics.mean(real_times)
     
     minutes_result = int(average_real_time / 60)
-    seconds_result = average_real_time % 60
+    seconds_result = round(average_real_time % 60, 3)
+    if i==0:
+        serial_time = average_real_time
 
-    print(f"n_proc={i+1}, n_exec={num_runs}, time: {minutes_result} min {seconds_result} s")
+    scale = round(serial_time / average_real_time, 3)
+    print(f"n_proc={i+1}, time: {minutes_result} min, {seconds_result} s, scale: {scale}")
--- assignment2/src/main.cpp
+++ assignment2/src/main.cpp
@@ -11,20 +11,20 @@
 
 using namespace std;
 
-// set the maximum size of the ngram
+// Set the maximum size of the ngram
 static constexpr size_t max_pattern_len = 3;
 static constexpr size_t max_dictionary_size = 128;
 static_assert(max_pattern_len > 1, "The pattern must contain at least one character");
 static_assert(max_dictionary_size > 1, "The dictionary must contain at least one element");
 
-// simple class to represent a word of our dictionary
+// Simple class to represent a word of our dictionary
 struct word {
   char ngram[max_pattern_len + 1];  // the string data, statically allocated
   size_t size = 0;                  // the string size
   size_t coverage = 0;              // the score of the word
 };
 
-// utility struct to ease working with words
+// Utility struct to ease working with words
 struct word_coverage_lt_comparator {
   bool operator()(const word &w1, const word &w2) const { return w1.coverage < w2.coverage; }
 };
@@ -32,7 +32,7 @@ struct word_coverage_gt_comparator {
   bool operator()(const word &w1, const word &w2) const { return w1.coverage > w2.coverage; }
 };
 
-// this is our dictionary of ngram
+// This is our dictionary of ngram
 struct dictionary {
   vector<word> data;
   vector<word>::iterator worst_element;
@@ -75,9 +75,23 @@ size_t count_coverage(const string &dataset, const char *ngram) {
   return counter * ngram_size;
 }
 
+// Create the MPI_Datatype for the word structure
+void createMPIWordType(MPI_Datatype* mpiWordType) {
+  int blocklengths[3] = {max_pattern_len + 1, 1, 1};
+  MPI_Datatype types[3] = {MPI_CHAR, MPI_UNSIGNED_LONG, MPI_UNSIGNED_LONG};
+  MPI_Aint offsets[3];
+
+  offsets[0] = offsetof(word, ngram);
+  offsets[1] = offsetof(word, size);
+  offsets[2] = offsetof(word, coverage);
+
+  MPI_Type_create_struct(3, blocklengths, offsets, types, mpiWordType);
+  MPI_Type_commit(mpiWordType);
+}
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-  // initialize
+
+  // Initialize
   int provided_thread_level;
   const int rc_init = MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided_thread_level);
   exit_on_fail(rc_init);
@@ -86,7 +100,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
       return EXIT_FAILURE;
   }
 
-  // set context
+  // Set context
   int size;
   const int rc_size = MPI_Comm_size(MPI_COMM_WORLD, &size);
   exit_on_fail(rc_size);
@@ -94,21 +108,20 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
   const int rc_rank = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   exit_on_fail(rc_rank);
 
-  // read the whole database of SMILES and put them in a single string
-  // NOTE: we can figure out which is our alphabet
-
-  // data structures that every process need to have
+  // Data structures that every process need to have
   unordered_set<char> alphabet_builder;
   string database;
   int db_size = 0;
   vector<char> alphabet;
   int alphabet_size = 0;
 
-  // only the process P0 read the input and compute initial stuff
+  // Only the process P0 read the input and compute initial stuff
+  // Using a producer/consumer style
   if(rank==0){
     cerr << "Reading the molecules from the standard input ..." << endl;
-
-    // compute the alphabet and the database
+    // Read the whole database of SMILES and put them in a single string
+    // NOTE: we can figure out which is our alphabet
+    // Compute the alphabet and the database
     database.reserve(209715200);  // 200MB
     for (string line; getline(cin, line);
           /* automatically handled */) {
@@ -118,7 +131,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
       }
     }
 
-    // put the alphabet in a container with random access capabilities
+    // Put the alphabet in a container with random access capabilities
     alphabet_builder.reserve(alphabet_builder.size());
     for_each(begin(alphabet_builder), end(alphabet_builder),
                   [&alphabet](const auto character) { alphabet.push_back(character); });
@@ -127,7 +140,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     alphabet_size = alphabet.size();
   }
 
-  // send in broadcast the alphabet and its size
+  // Send in broadcast the alphabet and its size
   const int rc_alpha_size = MPI_Bcast(&alphabet_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
   exit_on_fail(rc_alpha_size);
 
@@ -135,7 +148,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
   const int rc_alphabet = MPI_Bcast(alphabet.data(), alphabet.size(), MPI_CHAR, 0, MPI_COMM_WORLD);
   exit_on_fail(rc_alphabet);
 
-  // send in broadcast the database and its size
+  // Send in broadcast the database and its size
   const int rc_db_size = MPI_Bcast(&db_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
   exit_on_fail(rc_db_size);
 
@@ -143,7 +156,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
   const int rc_database = MPI_Bcast(database.data(), database.size(), MPI_CHAR, 0, MPI_COMM_WORLD);
   exit_on_fail(rc_database);
   
-  // precompute the number of permutations according to the number of characters (all processes for now compute the number of permutations)
+  // Precompute the number of permutations according to the number of characters
   auto permutations = vector(max_pattern_len, alphabet.size());
   for (size_t i{1}; i < permutations.size(); ++i) {
     permutations[i] = alphabet.size() * permutations[i - size_t{1}];
@@ -158,108 +171,77 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     total_ngrams[i] = permutations[i];
     ngrams_per_process[i] = (total_ngrams[i] + size - 1) / size;
     start_index[i] = rank * ngrams_per_process[i];
-    end_index[i] = min((rank + 1) * ngrams_per_process[i] - 1, total_ngrams[i]-1);  //se metto 3 o 4 processi, cambia l'end index e quindi va out of bound
+    end_index[i] = min((rank + 1) * ngrams_per_process[i], total_ngrams[i]) - 1;
   }
-
-  //cerr << "Process P" << rank << endl << "Total ngram1: " << total_ngrams[0] << "  Total ngram2: " << total_ngrams[1] << "  Total ngram3: " << total_ngrams[2] << endl << "Ngrams per process1: " << ngrams_per_process[0] << "  Ngrams per process2: " << ngrams_per_process[1] << "  Ngrams per process3: " << ngrams_per_process[2] << endl << "Start index1: " << start_index[0] << "  Start index2: " << start_index[1] << "  Start index3: " << start_index[2] << endl << "End index1: " << end_index[0] << "  End index2: " << end_index[1] << "  End index3: " << end_index[2] << endl;
-  // declare the dictionary that holds all the ngrams with the greatest coverage of the dictionary
+ 
+  // Declare the dictionary that holds all the ngrams with the greatest coverage of the dictionary
   dictionary result;
 
+  MPI_Datatype mpiWordType;
+  createMPIWordType(&mpiWordType);
+
+  // Compute the ngrams and their coverage
   for (size_t ngram_size{1}; ngram_size <= max_pattern_len; ++ngram_size) {
-    vector<char> all_words;
-    vector<int> all_cov;
     if(rank==0){
-      all_words.resize(total_ngrams[ngram_size-1] * ngram_size);
-      all_cov.resize(total_ngrams[ngram_size-1]);
+      cerr << "Computing ngrams of size " << ngram_size << " and their coverage ..." << endl;
     }
-    vector<char> words;           // vector containing the permutations
-    vector<int> cov;              // vector containing the coverage for each permutation, in order
+    vector<word> all_words;
+    vector<word> words;           
 
     // Distribute work among MPI processes
-    // if not P0, put into 2 vectors to send them later to P0
-    if(rank==0){
-      // insert directly into the dictionary
-      for (size_t word_index = start_index[ngram_size-1]; word_index <= end_index[ngram_size-1]; ++word_index) {
-        // compose the ngram
-        word current_word;
-        memset(current_word.ngram, '\0', max_pattern_len + 1);
-        for (size_t character_index{0}, remaining_size = word_index; character_index < ngram_size;
-                ++character_index, remaining_size /= alphabet.size()) {
-            current_word.ngram[character_index] = alphabet[remaining_size % alphabet.size()];
-      }
-      // transform into a vector of char
-      for (int i = 0; current_word.ngram[i] != '\0'; ++i) {
-          words.push_back(current_word.ngram[i]);
+    for (size_t word_index = start_index[ngram_size-1]; word_index <= end_index[ngram_size-1]; ++word_index) {
+      // Compose the ngram
+      word current_word;
+      memset(current_word.ngram, '\0', max_pattern_len + 1);
+      for (size_t character_index{0}, remaining_size = word_index; character_index < ngram_size;
+              ++character_index, remaining_size /= alphabet.size()) {
+          current_word.ngram[character_index] = alphabet[remaining_size % alphabet.size()];
       }
 
       current_word.size = ngram_size;
-
-      // Evaluate the coverage and add the word to the dictionary
       current_word.coverage = count_coverage(database, current_word.ngram);
-      cov.push_back(current_word.coverage);   // push its coverage into the cov vector
-      result.add_word(current_word);
-      }
-    }else{
-      // Don't put into the dictionary
-      for (size_t word_index = start_index[ngram_size-1]; word_index <= end_index[ngram_size-1]; ++word_index) {
-        // compose the ngram
-        word current_word;
-        memset(current_word.ngram, '\0', max_pattern_len + 1);
-        for (size_t character_index{0}, remaining_size = word_index; character_index < ngram_size;
-                ++character_index, remaining_size /= alphabet.size()) {
-            current_word.ngram[character_index] = alphabet[remaining_size % alphabet.size()];
-        }
-        // Transform into a vector of char
-        for (int i = 0; current_word.ngram[i] != '\0'; ++i) {
-            words.push_back(current_word.ngram[i]);
-        }
-
-        current_word.size = ngram_size;
-
-        // Evaluate the coverage and push into the cov vector
-        current_word.coverage = count_coverage(database, current_word.ngram);
-        cov.push_back(current_word.coverage);
-      }
+      words.push_back(current_word);
     }
-    // Gather to P0 all the computed permutations and coverages
-    //const int rc_gather_words = MPI_Gather(words.data(), ngrams_per_process[ngram_size-1]*ngram_size, MPI_CHAR, all_words.data(), ngrams_per_process[ngram_size-1]*ngram_size, MPI_CHAR, 0, MPI_COMM_WORLD);
-    const int rc_gather_words = MPI_Gather(words.data(), words.size(), MPI_CHAR, all_words.data(), words.size(), MPI_CHAR, 0, MPI_COMM_WORLD);
-    exit_on_fail(rc_gather_words);
 
-    //const int rc_gather_cov = MPI_Gather(cov.data(), ngrams_per_process[ngram_size-1], MPI_INT, all_cov.data(), ngrams_per_process[ngram_size-1], MPI_INT, 0, MPI_COMM_WORLD);
-    const int rc_gather_cov = MPI_Gather(cov.data(), cov.size(), MPI_INT, all_cov.data(), cov.size(), MPI_INT, 0, MPI_COMM_WORLD);
-    exit_on_fail(rc_gather_cov);
+    /*
+    * Uncomment this part to skip useless work (sending all the ngrams computed by each process),
+    * but this leads to an output file that can be different in the order of ngrams depending on 
+    * the number of processors used
+    */
+    // Sort and remove whats beyond max_dictionary_size, keep only the ngrams with highest coverage
+    /*std::sort(words.begin(), words.end(), compareByCoverage);
+    if (words.size() > max_dictionary_size) {
+      words.erase(words.begin() + max_dictionary_size, words.end());
+    }*/
 
     if(rank==0){
-      for(size_t k=ngrams_per_process[ngram_size-1]; k<total_ngrams[ngram_size-1]; k++){
-        int idx = k*ngram_size;
-        word current_word;
-        memset(current_word.ngram, '\0', max_pattern_len + 1);
-        for(size_t h=0; h<ngram_size; h++){
-            current_word.ngram[h] = all_words[idx+h]; 
-        }
-        current_word.size = ngram_size;
-        current_word.coverage = all_cov[k];
-        result.add_word(current_word);
-      }
+      all_words.resize(words.size() * size);
     }
 
-    // Clear for next iteration
-    all_words.clear();
-    all_cov.clear();
-    words.clear();
-    cov.clear();
+    // Gather to P0 all the computed ngrams and respective coverages
+    const int rc_gather_words = MPI_Gather(words.data(), words.size(), mpiWordType, all_words.data(), words.size(), mpiWordType, 0, MPI_COMM_WORLD);
+    exit_on_fail(rc_gather_words);
+
+    // Only P0 populates the dictionary
+    if(rank==0){
+      for(auto w : all_words){
+        result.add_word(w);
+      }
+    }
   }
 
-  // generate the final dictionary
+  // Generate the final dictionary
   // NOTE: we sort it for pretty-printing
   if(rank==0){
     cout << "NGRAM COVERAGE" << endl;
     sort(begin(result.data), end(result.data), word_coverage_gt_comparator{});
     result.write(cout);
+    cerr << "Final dictionary printed in the output file" << endl;
   }
 
-  // finalize
+  MPI_Type_free(&mpiWordType);
+
+  // Finalize
   const int rc_finalize = MPI_Finalize();
   exit_on_fail(rc_finalize);
   return EXIT_SUCCESS;
